b94606a3da847dbe9734efc5cf72f63b8d397f06
Dror Asaf <drorasaf87@gmail.com>
2017-04-11 01:07:01 +0000

    fix docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,8 +32,8 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.coverage',
-    'sphinx.ext.githubpages']
+              'sphinx.ext.coverage',
+              'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -85,7 +85,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -154,6 +154,3 @@ texinfo_documents = [
      author, 'mongotriggers', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
