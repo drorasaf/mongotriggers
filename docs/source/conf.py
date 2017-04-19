d162551af7e06411819acabfa619dd915cd62489
Dror Asaf <drorasaf87@gmail.com>
2017-04-19 01:34:31 +0000

    sort docs according to source

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.intersphinx']
 
 napoleon_google_docstring = True
+autodoc_member_order = 'bysource'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
